c0780f590842bd2f1b2db2b08cf2bbc3075e6e32
zoooooo <n_shimazu@innovation.co.jp>
2020-06-08 16:38:40 +0900

    csrfサンプルを追加しました

--- www/html/login.php
+++ www/html/login.php
@@ -1,11 +1,17 @@
 <?php
+// 定数ファイル読み込み
 require_once '../conf/const.php';
+// 関数ファイル読み込み
 require_once MODEL_PATH . 'functions.php';
 
+// セッション開始
 session_start();
 
+// ログインが成功した場合
 if(is_logined() === true){
+  // 商品一覧ページに移動
   redirect_to(HOME_URL);
 }
 
+// viewファイル読み込み
 include_once VIEW_PATH . 'login_view.php';
